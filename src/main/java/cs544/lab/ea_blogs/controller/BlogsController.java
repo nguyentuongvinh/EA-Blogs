92f747724e164c9e23bc445a7742deb6e9696963
nguyentuongvinh <nguyentuongvinh@gmail.com>
2016-04-16 15:07:10 -0500

    Revise

--- src/main/java/cs544/lab/ea_blogs/controller/BlogsController.java
+++ src/main/java/cs544/lab/ea_blogs/controller/BlogsController.java
@@ -7,14 +7,17 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import cs544.lab.ea_blogs.model.User;
 import cs544.lab.ea_blogs.repository.UserRepository;
+import cs544.lab.ea_blogs.service.ArticleService;
+import cs544.lab.ea_blogs.service.CommentService;
+import cs544.lab.ea_blogs.service.UserService;
 
 /**
  * Handles requests for the application home page.
@@ -23,7 +26,14 @@ import cs544.lab.ea_blogs.repository.UserRepository;
 public class BlogsController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BlogsController.class);
+	@Autowired
+	private ArticleService articleService;
 	
+	@Autowired
+	private CommentService commentService;
+	
+	@Autowired
+	private UserService userService;
 
 	@Resource
 	private UserRepository userDao;
@@ -38,9 +48,9 @@ public class BlogsController {
 	public String userList(Model model) {
 		logger.info("Get all users:");
 
-		List<User> users = userDao.findByUsername("reader");
+		List<User> users = userService.findByUsername("reader");
 		for (User user : users)
-			logger.info(user.getUsername());
+			logger.info(user.getPassword());
 		
 		return "main";
 	}
