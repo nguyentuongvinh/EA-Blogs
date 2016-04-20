2d20173c739d3b181421c7193882c0949478c9cc
nguyentuongvinh <nguyentuongvinh@gmail.com>
2016-04-20 18:37:48 -0500

    Add scheduling

---
+++ src/main/java/cs544/lab/ea_blogs/controller/SchedulingReport.java
@@ -0,0 +1,25 @@
+package cs544.lab.ea_blogs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class SchedulingReport {
+	@Autowired
+	private MailSender mailSender;
+	
+//	@Scheduled(cron="0 0/1 * * * *")
+	@Scheduled(cron="0 0 24 * * *")
+	public void reportNumOfArticlesPosted() {
+		System.out.println("Scheduling is running");
+		
+		SimpleMailMessage smm = new SimpleMailMessage();
+		smm.setFrom("eablobtest@gmail.com");
+		smm.setTo("phaolovinh@gmail.com");
+		smm.setSubject("EA Blog Article Report");
+		smm.setText("Report...");
+		
+		mailSender.send(smm);
+	}
+}
